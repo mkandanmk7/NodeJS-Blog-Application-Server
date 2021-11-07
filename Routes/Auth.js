cb8867d86a61fe0d11443eb5c492419187d5a847
muthu <mkandanmk7@gmail.com>
2021-11-07 11:39:59 +0530

    bcrypt done

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,20 +1,23 @@
 const router = require("express").Router();
-
+const bcrypt = require("bcrypt");
 const User = require("../Models/User");
 
 //register
 router.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPass,
     });
     const user = await newUser.save();
     res
       .status(200)
       .send({ register: "user registered successfully", details: user });
   } catch (err) {
+    console.log(err);
     res.status(500).send({ Error: "Error in register", error: err });
   }
 });
