6ab6ae465ae75ee943a361354be847e3c19f7b08
muthu <mkandanmk7@gmail.com>
2021-11-25 10:31:57 +0530

    order

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const authRoute = require("./Routes/Auth");
 const userRoute = require("./Routes/Users");
 const postRoute = require("./Routes/Posts");
 const categoryRoute = require("./Routes/Categories");
-
+app.use(cors());
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
 const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -35,11 +37,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.use(cors());
-app.use(express.json());
-
 // make public folder:
-app.use("/images", express.static(path.join(__dirname, "/images")));
 
 app.use("/api/upload", upload.single("file"), (req, res) => {
   try {
